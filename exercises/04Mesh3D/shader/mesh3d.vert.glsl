5304416ad3f257cf1dbb8eeb9542f4632d48503d
Katharina Bork <katha.bork75@gmail.com>
2022-11-17 09:17:41 +0100

    rename 2d -> 3d

---
+++ exercises/04Mesh3D/shader/mesh3d.vert.glsl
@@ -0,0 +1,16 @@
+#version 300 es
+precision highp float;
+
+layout(location = 0) in vec3 a_position;
+layout(location= 1) in vec3 a_color;
+out vec3 v_color;
+uniform mat3 mat3_transform;
+
+void main()
+{
+    //gl_Position = vec4(a_position.xyz, 1);
+    gl_Position = vec4(a_position.xyz, 1.0);
+    v_color = a_color;
+
+    
+}
