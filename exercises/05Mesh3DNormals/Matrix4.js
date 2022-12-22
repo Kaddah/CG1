7b2fac5d495ab14ae0565b524a28fdd4ed77b8ce
Katharina Bork <katha.bork75@gmail.com>
2022-12-22 09:33:37 +0100

    Lab05 2d

--- exercises/05Mesh3DNormals/Matrix4.js
+++ exercises/05Mesh3DNormals/Matrix4.js
@@ -124,10 +124,10 @@ export let Matrix4 = {
      */
      transpose: function (a) {
         // Lab 05, 2(d)
-        return [1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1];
+        return [a[0], a[4], a[8], a[12],
+                a[1], a[5], a[9], a[13],
+                a[2], a[6], a[10], a[14],
+                a[3], a[7], a[11], a[15]];
         
     }
 
